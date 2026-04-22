b9de1a0bfafb27f63a9bde3fbc0d8c9ebccf3f0b
dumpkids <dumpkids09@gmail.com>
2026-04-22 12:25:40 +0700

    Update setupmirrortools.sh

--- setupmirrortools.sh
+++ setupmirrortools.sh
@@ -23,7 +23,6 @@ main() {
     mkdir -p "${MIRRORTOOL_DIR}/temp"
     mkdir -p "${MIRRORTOOL_DIR}/dir"
     mkdir -p "${MIRRORTOOL_DIR}/dir/download"
-    mkdir -p "${MIRRORTOOL_DIR}/ep9"
 
     # Alihkan output ke file log sekaligus tampil di layar
     exec > >(tee -a "$SETUP_LOG") 2>&1
